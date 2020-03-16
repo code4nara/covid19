e08f5bfa0690a8871f568766ed301edde1b5d936
heimax222 <35659901+heimax222@users.noreply.github.com>
2020-03-16 23:20:56 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -26,6 +26,7 @@
           :chart-data="patientsGraph"
           :date="Data.patients.date"
           :unit="'人'"
+          :defaultDataKind="'cumulative'"
           :url="'http://www.pref.nara.jp/1652.htm'"
         />
       </v-col>
