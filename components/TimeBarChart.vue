324f76c665c881f216d46110fcf92a42008d137f
heimax222 <35659901+heimax222@users.noreply.github.com>
2020-03-23 20:58:47 +0900

    Update TimeBarChart.vue

--- components/TimeBarChart.vue
+++ components/TimeBarChart.vue
@@ -64,7 +64,7 @@ export default {
       required: false,
       default: ''
     },
-    default-datakind: {
+    defaultdatakind: {
       type: String,
       required: false,
       default: 'transition'
@@ -72,7 +72,7 @@ export default {
   },
   data() {
     return {
-      dataKind: this.default-datakind
+      dataKind: this.defaultdatakind
     }
   },
   computed: {
