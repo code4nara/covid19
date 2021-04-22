4d4521218c7829cadf8bbcc0077006f2790561e4
Yasushi IS <6837346+YasushiIS@users.noreply.github.com>
2021-04-22 15:51:32 +0900

    残り病床数のマイナス対応。ソースないの data/data.json の更新 (#306)
    * update data.json, add inspection_persons_summary{}
    
    * update workdflow, not to use naracity-data, remove unnecessarily yml
    
    * Update data/data.json
    
    * 入院者数の状況表の修正と2020/1/12項目変更対応
    
    * Add 入院療養中数 and 調整中 to formatConfirmedCases.ts
    
    * #293 Update circle-chart for data update on 2021/1/12
    
    * Modifyed Popup in CiecleChart 病床室数
    
    * update data.json
    
    * Update Comment and reduce css error v-sanitaize
    
    Co-authored-by: Yasushi ISHIZUKA <ishizuka@zukatech.com>

--- components/InspectionPersonsChart.vue
+++ components/InspectionPersonsChart.vue
@@ -22,7 +22,6 @@
         <li
           v-for="remarks_text in remarks"
           :key="remarks_text"
-          v-sanitaize
           v-html="remarks_text"
         />
         <!-- eslint-disable vue/no-v-html -->
