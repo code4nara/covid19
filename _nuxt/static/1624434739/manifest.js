b49b250fb13deb50898065525a03883317999eac
codefornara <codefornara@users.noreply.github.com>
2021-06-24 06:22:53 +0000

    deploy: 8b34939d21c5e8de86ffa86f8269d08a5dbb0f8a

--- _nuxt/static/1624434739/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fprint\u002Fflow","\u002Fabout","\u002Fflow","\u002Fgovernment","\u002Fmunicipalities","\u002Fworker","\u002Fnaracity","\u002Fparent"]})
